4a59a4b6f8ca1040bef801462c03c26e06e6ed2b
Amar Singh <amar.jrschbs@gmail.com>
2022-10-04 19:24:03 +0530

    Add files via upload

--- src/Conn.java
+++ src/Conn.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Conn {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    Conn(){
+        try {
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs", "root", "Amar@1995");
+            s=c.createStatement();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/Project.java
+++ src/Project.java
@@ -1,2 +1,238 @@
-package PACKAGE_NAME;public class Project {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
+public class Project extends JFrame implements ActionListener {
+     String atype,meter;
+    Project(String atype, String meter){
+        this.atype=atype;
+        this.meter=meter;
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icon/elect1.jpg"));
+        Image i2=i1.getImage().getScaledInstance(1500,850,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        add(image);
+
+        JMenuBar mb=new JMenuBar();
+        setJMenuBar(mb);
+
+        JMenu master=new JMenu("Master");
+        master.setForeground(Color.BLUE);
+
+
+
+        JMenuItem newcustomer=new JMenuItem("New Customer");
+        newcustomer.setFont(new Font("monospaced",Font.PLAIN,12));
+        newcustomer.setBackground(Color.WHITE);
+        ImageIcon icon1=new ImageIcon(ClassLoader.getSystemResource("icon/icon1.png"));
+        Image image1=icon1.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        newcustomer.setIcon(new ImageIcon(image1));
+        newcustomer.addActionListener(this);
+        newcustomer.setMnemonic('D');
+        newcustomer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+        master.add(newcustomer);
+
+        JMenuItem customerdetails=new JMenuItem("Customer Details");
+        customerdetails.setFont(new Font("monospaced",Font.PLAIN,12));
+        customerdetails.setBackground(Color.WHITE);
+        ImageIcon icon2=new ImageIcon(ClassLoader.getSystemResource("icon/icon2.png"));
+        Image image2=icon2.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        customerdetails.setIcon(new ImageIcon(image2));
+        customerdetails.addActionListener(this);
+        customerdetails.setMnemonic('M');
+        customerdetails.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
+        master.add(customerdetails);
+
+        JMenuItem depositdetails=new JMenuItem("Deposit Details");
+        depositdetails.setFont(new Font("monospaced",Font.PLAIN,12));
+        depositdetails.setBackground(Color.WHITE);
+        ImageIcon icon3=new ImageIcon(ClassLoader.getSystemResource("icon/icon3.png"));
+        Image image3=icon3.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        depositdetails.setIcon(new ImageIcon(image3));
+        depositdetails.addActionListener(this);
+        depositdetails.setMnemonic('N');
+        depositdetails.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        master.add(depositdetails);
+
+        JMenuItem claculatebill=new JMenuItem("Calculate Bill");
+        claculatebill.setFont(new Font("monospaced",Font.PLAIN,12));
+        claculatebill.setBackground(Color.WHITE);
+        ImageIcon icon4=new ImageIcon(ClassLoader.getSystemResource("icon/icon4.png"));
+        Image image4=icon4.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        claculatebill.setIcon(new ImageIcon(image4));
+        claculatebill.addActionListener(this);
+        claculatebill.setMnemonic('B');
+        claculatebill.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
+        master.add(claculatebill);
+
+        JMenu info=new JMenu("Information");
+        info.setForeground(Color.RED);
+
+
+        JMenuItem updateInfo=new JMenuItem("Update Information");
+        updateInfo.setFont(new Font("monospaced",Font.PLAIN,12));
+        updateInfo.setBackground(Color.WHITE);
+        ImageIcon icon5=new ImageIcon(ClassLoader.getSystemResource("icon/icon5.png"));
+        Image image5=icon5.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        updateInfo.setIcon(new ImageIcon(image5));
+        updateInfo.addActionListener(this);
+        updateInfo.setMnemonic('U');
+        updateInfo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, ActionEvent.CTRL_MASK));
+        info.add(updateInfo);
+
+        JMenuItem viewInfo=new JMenuItem("View Information");
+        viewInfo.setFont(new Font("monospaced",Font.PLAIN,12));
+        viewInfo.setBackground(Color.WHITE);
+        ImageIcon icon6=new ImageIcon(ClassLoader.getSystemResource("icon/icon6.png"));
+        Image image6=icon6.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        viewInfo.setIcon(new ImageIcon(image6));
+        viewInfo.addActionListener(this);
+        viewInfo.setMnemonic('V');
+        viewInfo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+        info.add(viewInfo);
+
+        JMenu user=new JMenu("User");
+        user.setForeground(Color.BLUE);
+
+
+        JMenuItem payBill=new JMenuItem("Pay Bill");
+        payBill.setFont(new Font("monospaced",Font.PLAIN,12));
+        payBill.setBackground(Color.WHITE);
+        ImageIcon icon7=new ImageIcon(ClassLoader.getSystemResource("icon/icon4.png"));
+        Image image7=icon7.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        payBill.setIcon(new ImageIcon(image7));
+        payBill.addActionListener(this);
+        payBill.setMnemonic('P');
+        payBill.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+        user.add(payBill);
+
+        JMenuItem billDetails=new JMenuItem("Bill Details");
+        billDetails.setFont(new Font("monospaced",Font.PLAIN,12));
+        billDetails.setBackground(Color.WHITE);
+        ImageIcon icon8=new ImageIcon(ClassLoader.getSystemResource("icon/icon6.png"));
+        Image image8=icon8.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        billDetails.setIcon(new ImageIcon(image8));
+        billDetails.addActionListener(this);
+        billDetails.setMnemonic('B');
+        billDetails.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
+        user.add(billDetails);
+
+        JMenu report=new JMenu("Report");
+        report.setForeground(Color.RED);
+
+
+        JMenuItem generateBill=new JMenuItem("Generate Bill");
+        generateBill.setFont(new Font("monospaced",Font.PLAIN,12));
+        generateBill.setBackground(Color.WHITE);
+        ImageIcon icon9=new ImageIcon(ClassLoader.getSystemResource("icon/icon7.png"));
+        Image image9=icon9.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        generateBill.setIcon(new ImageIcon(image9));
+        generateBill.addActionListener(this);
+        generateBill.setMnemonic('G');
+        generateBill.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G, ActionEvent.CTRL_MASK));
+        report.add(generateBill);
+
+        JMenu utility=new JMenu("Utility");
+        utility.setForeground(Color.BLUE);
+
+
+        JMenuItem notepad=new JMenuItem("Notepad");
+        notepad.setFont(new Font("monospaced",Font.PLAIN,12));
+        notepad.setBackground(Color.WHITE);
+        ImageIcon icon10=new ImageIcon(ClassLoader.getSystemResource("icon/icon12.png"));
+        Image image10=icon10.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        notepad.setIcon(new ImageIcon(image10));
+        notepad.addActionListener(this);
+        notepad.setMnemonic('T');
+        notepad.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, ActionEvent.CTRL_MASK));
+        utility.add(notepad);
+
+        JMenuItem calculator=new JMenuItem("Calculator");
+        calculator.setFont(new Font("monospaced",Font.PLAIN,12));
+        calculator.setBackground(Color.WHITE);
+        ImageIcon icon11=new ImageIcon(ClassLoader.getSystemResource("icon/icon12.png"));
+        Image image11=icon11.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        calculator.setIcon(new ImageIcon(image11));
+        calculator.addActionListener(this);
+        calculator.setMnemonic('C');
+        calculator.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        utility.add(calculator);
+
+        JMenu Exit=new JMenu("Exit");
+        Exit.setForeground(Color.RED);
+
+
+        JMenuItem exit=new JMenuItem("Exit");
+        exit.setFont(new Font("monospaced",Font.PLAIN,12));
+        exit.setBackground(Color.WHITE);
+        ImageIcon icon12=new ImageIcon(ClassLoader.getSystemResource("icon/icon12.png"));
+        Image image12=icon12.getImage().getScaledInstance(20,20,Image.SCALE_DEFAULT);
+        exit.setIcon(new ImageIcon(image12));
+        exit.addActionListener(this);
+        exit.setMnemonic('W');
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK));
+        Exit.add(exit);
+
+       if(atype.equals("Admin")){
+        mb.add(master);
+
+       }else {
+           mb.add(info);
+           mb.add(user);
+           mb.add(report);
+       }
+        mb.add(utility);
+        mb.add(Exit);
+
+        setLayout(new FlowLayout());
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        String msg=ae.getActionCommand();
+        if(msg.equals("New Customer")){
+            new NewCustomer();
+        }else if(msg.equals("Customer Details")){
+            new CustomerDetails();
+        }else if(msg.equals("Deposit Details")){
+            new DepositDetails();
+        }else if(msg.equals("Calculate Bill")){
+            new CalculateBill();
+        }else if(msg.equals("View Information")){
+            new ViewInformation(meter);
+        }else if(msg.equals("Update Information")){
+            new UpdateInformation(meter);
+        }else if(msg.equals("Bill Details")){
+            new BillDetails(meter);
+        }else if(msg.equals("Pay Bill")){
+            new PayBill(meter);
+        }else if(msg.equals("Notepad")){
+            try{
+                Runtime.getRuntime().exec("notepad");
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else if(msg.equals("Calculator")){
+            try{
+                Runtime.getRuntime().exec("Calculator");
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else if(msg.equals("Exit")){
+            setVisible(false);
+            new Login();
+        }else if(msg.equals("Generate Bill")){
+            new GenerateBill(meter);
+        }
+    }
+    public static void main(String[] args) {
+        new Project("","");
+    }
 }
